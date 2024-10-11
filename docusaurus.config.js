01aa3e84fb81003f87025da6c8469852a26b5f0f
BloxLane <158101175+BloxLane@users.noreply.github.com>
2024-10-10 21:11:31 -0400

    hi

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://docs.southoakstores.com",
+  url: "https://ualqc.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/southoak",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
