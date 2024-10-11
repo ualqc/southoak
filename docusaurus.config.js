de73db8b717911faaf78f92e1e496672c5474305
BloxLane <158101175+BloxLane@users.noreply.github.com>
2024-10-10 21:15:17 -0400

    hi

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
         respectPrefersColorScheme: false,
       },
       navbar: {
-        title: "BloxLane Support",
+        title: "south oak support",
         items: [],
       },
       footer: {
